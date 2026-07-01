dcef8ec734e22415ab00b5c807de478c0ebb0244
elizabeth alfaro <ealfaro@cpp.edu>
2026-07-01 14:37:21 -0700

    meow

--- scripts/myscript2.R
+++ scripts/myscript2.R
@@ -9,3 +9,11 @@ ggplot(complete_old, aes(x=hindfoot_length, y=weight))+
 
 ggplot(complete_old, aes(x=hindfoot_length, y=weight, color = species))+
   geom_point(alpha = 0.2)
+
+#hi my name is holden
+#im a comp sci nerd
+#if i was an animal id be a guinie pig
+
+ggplot(complete_old, aes(y=hindfoot_length, x=weight, color = sex))+
+  geom_point(alpha = 0.2)
+
